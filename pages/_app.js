fa1d86b15416836dad9f3d02a1427304f9481dd3
Lass <leo_anjos1@hotmail.com>
2021-01-31 23:51:54 -0300

    update link bg and thumb

--- pages/_app.js
+++ pages/_app.js
@@ -40,13 +40,13 @@ export default function App({ Component, pageProps }) {
         <meta property="og:url" content="https://attackontitan-quiz.oleonardoanjos.vercel.app/" key="url" />
         <meta property="og:title" content={db.title} key="ogtitle" />
         <meta property="og:description" content={db.description} key="ogdescription" />
-        <meta property="og:image" content={db.bg} key="ogimage" />
+        <meta property="og:image" content={db.thumb} key="ogimage" />
 
         <meta property="twitter:card" content="summary_large_image" key="twittercard" />
         <meta property="twitter:url" content="https://attackontitan-quiz.oleonardoanjos.vercel.app/" key="twitterurl" />
         <meta property="twitter:title" content={db.title} key="twittertitle" />
         <meta property="twitter:description" content={db.description} key="twitterdescription" />
-        <meta property="twitter:image" content={db.bg} key="twitterimage" />
+        <meta property="twitter:image" content={db.thumb} key="twitterimage" />
 
         <link rel="icon" type="image/png" sizes="32x32" href={db.favicon} />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
